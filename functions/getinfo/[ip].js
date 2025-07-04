bc1701ee09979e8c5f39782118cb46d6ba1fcfd2
x-dr <xd9722@gmail.com>
2025-07-04 16:27:14 +0000

    fix

--- functions/getinfo/[ip].js
+++ functions/getinfo/[ip].js
@@ -6,12 +6,10 @@ export async function onRequest({ request, params, env }) {
    
     
 
-
-    const res = await fetch(`https://pro.ip-api.com/json/${ip}?key=${env.IPKEY}lang=zh-CN&fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query`)
+    const res = await fetch(`https://pro.ip-api.com/json/${ip}?key=${env.IPKEY}&lang=zh-CN&fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query`)
     const data = await res.json();
     if (data.status !== 'success') {
-        return new Response(JSON.stringify({ error: data.message ,
-            ip:  env.IPKEY
+        return new Response(JSON.stringify({ error: data.message || 'Failed to retrieve data'
         }), {
             status: 400,
             headers: {
