d4deb8ef2678dde0ea608241b0c228c498464c03
x-dr <xd9722@gmail.com>
2025-07-11 18:34:36 +0000

    s

--- functions/ip.js
+++ functions/ip.js
@@ -4,12 +4,15 @@ export async function onRequest({ request }) {
   let mtjson = {};
 
   try {
+    const mtres = await fetch(
+      `https://apimobile.meituan.com/locate/v2/ip/loc?rgeo=true&ip=${geo.clientIp}`
+    );
+    const mtlatlon = await mtres.json();
+
     const mtdata = await fetch(
-      `https://apimobile.meituan.com/group/v1/city/latlng/${geo.geo.latitude},${geo.geo.longitude}?tag=0`
+      `https://apimobile.meituan.com/group/v1/city/latlng/${mtlatlon.data.lat},${mtlatlon.data.lon}?tag=0`
     );
     mtjson = await mtdata.json();
-
-
 
   } catch (error) {
     console.log('获取美团城市数据失败：', error);
