979e42e9540b4dd49630a348ded2748ffdf95236
x-dr <xd9722@gmail.com>
2025-07-11 18:03:00 +0000

    s

--- functions/ip.js
+++ functions/ip.js
@@ -1,9 +1,24 @@
-export function onRequest({request}) {
+export async function onRequest({ request }) {
   const geo = request.eo;
+
+  let mtjson = {};
+
+  try {
+    const mtdata = await fetch(
+      `https://apimobile.meituan.com/group/v1/city/latlng/${geo.geo.latitude},${geo.geo.longitude}?tag=0`
+    );
+    mtjson = await mtdata.json();
+
+
+
+  } catch (error) {
+    console.log('获取美团城市数据失败：', error);
+  }
+
   const res = JSON.stringify({
     geo: geo,
+    meituan: mtjson || {},
   });
-
   return new Response(res, {
     headers: {
       'content-type': 'application/json; charset=UTF-8',
