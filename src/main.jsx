b832eac08d786024946a0943a228c2debb860283
x-dr <xd9722@gmail.com>
2025-07-19 13:15:34 +0000

    S

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter  } from 'react-router-dom';
 import '@ant-design/v5-patch-for-react-19';
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
