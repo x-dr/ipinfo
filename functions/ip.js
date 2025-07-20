397064a681bef9e10b58242b0de2b94920a5283e
x-dr <xd9722@gmail.com>
2025-07-20 13:29:19 +0000

    a

--- functions/ip.js
+++ functions/ip.js
@@ -1,15 +1,38 @@
 export async function onRequest({ request }) {
+  let clientIp;
+
+
+  if (request.method === 'OPTIONS') {
+    // 处理 CORS 预检请求
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    });
+  }
+
   const geo = request.eo;
+
+  if (request.method == 'POST') {
+    const body = await request.json();
+    clientIp = body.ip;
+  }
+  if (!clientIp) {
+    clientIp = geo.clientIp;
+  }
 
   let mtjson = {};
   let mtlatlng = {};
 
   try {
     const mtres = await fetch(
-      `https://apimobile.meituan.com/locate/v2/ip/loc?rgeo=true&ip=${geo.clientIp}`
+      `https://apimobile.meituan.com/locate/v2/ip/loc?rgeo=true&ip=${clientIp}`
     );
     mtlatlng = await mtres.json();
-    
+
 
     const mtdata = await fetch(
       `https://apimobile.meituan.com/group/v1/city/latlng/${mtlatlng.data.lat},${mtlatlng.data.lng}?tag=0`
@@ -26,10 +49,10 @@ export async function onRequest({ request }) {
     meituan: mtjson || {},
   });
   return new Response(res, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
   });
 }
